d7e0759ee3048c806a38b65a987b06b09a8873c2
AntonBeYt <143318322+AntonBeYt@users.noreply.github.com>
2024-08-21 13:32:40 +0200

    pathname implementation for rooms

--- app/[room-id]/page.tsx
+++ app/[room-id]/page.tsx
@@ -1,27 +1,30 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, usePathname } from "next/navigation";
 import { Button } from "../components/Button/Button";
 import { useEffect, useState } from "react";
 import { useSupabaseClient } from "../hooks/useSupabaseClient";
 
 export default function Room() {
-  const searchParams = useSearchParams();
-  const [roomCode, setRoomCode] = useState("");
+  const path = usePathname();
+  const roomCode = path.slice(1);
+  // const searchParams = useSearchParams();
+  // const [roomCode, setRoomCode] = useState("");
   const client = useSupabaseClient(roomCode);
+  console.log(client);
 
-  useEffect(() => {
-    if (searchParams.has("room-id")) {
-      setRoomCode(searchParams.get("room-id") ?? "");
-    }
-  }, [searchParams]);
+  // useEffect(() => {
+  //   if (searchParams.has("room-id")) {
+  //     setRoomCode(searchParams.get("room-id") ?? "");
+  //   }
+  // }, [searchParams]);
 
   return (
     <div>
       <h1>VÄLKOMMEN</h1>
 
       <p>{roomCode}</p>
-      <Button onClick={() => client.unlockedKey("siri & per", 1)} text="SEND" />
+      <Button onClick={() => client.unlockedKey("vampyr", 1)} text="SEND" />
     </div>
   );
 }
