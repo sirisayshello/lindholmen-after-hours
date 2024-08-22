64808a6d9f73b59808f70d7f7e2ee88d8dee88c3
sirisayshello <veckovis.salongen0p@icloud.com>
2024-08-22 09:21:44 +0200

    context provider for game

--- app/hooks/useSupabaseClient.tsx
+++ app/hooks/useSupabaseClient.tsx
@@ -1,5 +1,5 @@
 import { supabase } from "@/supabase/client";
-import { log } from "console";
+import { Teams, useGame } from "./useGame";
 
 type Events = "key_unlocked" | "join";
 
@@ -8,7 +8,18 @@ type UnlockedKeyPayload = {
   keyId: number;
 };
 
+type PlayerJoinedPayload = {
+  name: string;
+};
+
+type PlayerReadyPayload = {
+  name: string;
+  team: Teams;
+};
+
 export const useSupabaseClient = (roomId: string) => {
+  const game = useGame();
+
   // Join a room/topic. Can be anything except for 'realtime'.
   const channel = supabase.channel(roomId);
 
@@ -21,11 +32,27 @@ export const useSupabaseClient = (roomId: string) => {
     console.log(`The ${payload.team} has unlocked key ${payload.keyId}`);
   }
 
+  function handlePlayerJoined(payload: PlayerJoinedPayload) {
+    // handle the join
+    console.log(payload);
+
+    game.addPlayer({
+      name: payload.name,
+      isReady: false,
+      team: "tbd",
+      isHost: false,
+      isMe: false,
+    });
+  }
+
   // Subscribe to the Channel
   channel
     .on("broadcast", { event: "join" }, (payload) => messageReceived(payload))
     .on("broadcast", { event: "unlocked_key" }, (payload) =>
       keyUnlocked(payload.payload as unknown as UnlockedKeyPayload)
+    )
+    .on("broadcast", { event: "player_joined" }, (payload) =>
+      handlePlayerJoined(payload.payload as unknown as PlayerJoinedPayload)
     )
     .subscribe((status) => {
       if (status !== "SUBSCRIBED") {
@@ -35,11 +62,11 @@ export const useSupabaseClient = (roomId: string) => {
 
   // Join a room/topic. Can be anything except for 'realtime'.
 
-  const sendJoined = () => {
+  const sendJoined = (name: string) => {
     channel.send({
       type: "broadcast",
-      event: "join",
-      payload: { message: "hello, world" },
+      event: "player_joined",
+      payload: { name },
     });
   };
 
