f59fa4f3381d887e6744bd9cc7e18985ab60e889
AntonBeYt <143318322+AntonBeYt@users.noreply.github.com>
2024-08-23 10:33:27 +0200

    good and bad popups

--- app/components/Popup/Popup.tsx
+++ app/components/Popup/Popup.tsx
@@ -1,16 +1,24 @@
 import Image from "next/image";
 import { ReactNode } from "react";
 
-export const Popup = ({
-  children,
-  close,
-}: {
+type PopupProps = {
+  goodIsVisible?: boolean;
+  badIsVisible?: boolean;
+  crystalIsVisible?: boolean;
   children: ReactNode;
   close: () => void;
-}) => {
+};
+
+export const Popup = ({
+  goodIsVisible,
+  badIsVisible,
+  crystalIsVisible,
+  children,
+  close,
+}: PopupProps) => {
   return (
     <div className="absolute top-36 flex flex-col items-center">
-      <div className="w-11/12 relative z-30 bg-gray-100 py-20 rounded-md text-black">
+      <div className="w-11/12 relative z-30 bg-gray-100 py-20 rounded-md text-black p-4 flex flex-col items-center justify-center gap-2">
         <div className="absolute top-2 right-2" onClick={close}>
           <Image src="/x.svg" width={26} height={26} alt="close" />
         </div>
